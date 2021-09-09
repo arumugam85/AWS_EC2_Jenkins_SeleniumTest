d71634ebf91486dc39eea1d2eb03b59ba62b4b75
arumugam85 <rarumugambe@gmail.com>
2021-09-08 21:39:34 -0400

    Test method updated

--- src/test/java/com/qascript/SeleniumAWSJenkinsDemo.java
+++ src/test/java/com/qascript/SeleniumAWSJenkinsDemo.java
@@ -1,16 +1,11 @@
 package com.qascript;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumAWSJenkinsDemo {
 
